350fdaa7ed1db433fea4255a709fa0e3c23b8fc6
martinhodde <martinhodde271@gmail.com>
2024-12-02 22:22:26 -0800

    Day 1 part 2

--- AoC2024/src/solutions/day1.rs
+++ AoC2024/src/solutions/day1.rs
@@ -1,7 +1,8 @@
+use counter::Counter;
 use std::collections::BinaryHeap;
 use super::utils::lines_from_file;
 
-pub fn solve_part_1() {
+fn get_sorted_lists() -> (Vec<u32>, Vec<u32>) {
     let filepath = "inputs/day1.txt";
     let lines = lines_from_file(filepath).expect(&format!("Input file {filepath} should exist"));
 
@@ -9,21 +10,25 @@ pub fn solve_part_1() {
     let mut heap1: BinaryHeap<u32> = BinaryHeap::new();
     let mut heap2: BinaryHeap<u32> = BinaryHeap::new();
     for line in lines {
-        let vals: Vec<u32> = line.split("   ").map(|s| s.parse().unwrap()).collect();
+        let vals: Vec<u32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
         heap1.push(vals[0]);
         heap2.push(vals[1]);
     }
 
-    // Convert to vectors
-    let vec1 = heap1.into_sorted_vec();
-    let vec2 = heap2.into_sorted_vec();
+    // Convert to sorted vectors
+    (heap1.into_sorted_vec(), heap2.into_sorted_vec())
+}
 
-    // Compute the element-wise absolute difference between the two vectors
-    let abs_diff_vec: Vec<u32> = vec1.iter().zip(vec2.iter()).map(|(&v1, &v2)| v1.abs_diff(v2)).collect();
-    let sum: u32 = abs_diff_vec.into_iter().sum();
+pub fn solve_part_1() {
+    // Compute the element-wise absolute difference between the two lists, then sum over the result
+    let (list1, list2) = get_sorted_lists();
+    let sum: u32 = list1.iter().zip(list2.iter()).map(|(&v1, &v2)| v1.abs_diff(v2)).sum();
     println!("Total distance between lists: {sum}")
 }
 
 pub fn solve_part_2() {
-    println!("Hello, world!")
+    let (list1, list2) = get_sorted_lists();
+    let list2_ctr = list2.iter().collect::<Counter<_, u32>>();
+    let sum: u32 = list1.iter().map(|n| n * list2_ctr[n]).sum();
+    println!("Similarity score between lists: {sum}")
 }
